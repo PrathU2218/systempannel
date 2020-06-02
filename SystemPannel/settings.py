cb477ea82cd7cde7d59c83bec8bf27351dc9cb9d
Prathmesh Namdeo <prathmesh.namdeo22@gmail.com>
2020-06-02 23:52:37 +0530

    prathmesh

--- SystemPannel/settings.py
+++ SystemPannel/settings.py
@@ -45,9 +45,11 @@ INSTALLED_APPS = [
     'user.apps.UserConfig',
     'rest_framework',
     'phone_field',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -56,6 +58,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'SystemPannel.urls'
 
