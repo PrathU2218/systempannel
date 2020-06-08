84ca387c4d8f5351c6ff485f3e355887191d162d
Prathmesh Namdeo <prathmesh.namdeo22@gmail.com>
2020-06-08 20:12:22 +0530

    today

--- SystemPannel/settings.py
+++ SystemPannel/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'user.apps.UserConfig',
     'video.apps.VideoConfig',
+    'questions.apps.QuestionsConfig',
     'rest_framework',
     'phone_field',
     'corsheaders',
